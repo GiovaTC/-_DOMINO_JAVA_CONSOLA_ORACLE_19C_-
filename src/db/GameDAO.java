8c70f3b54d1cf50f26917d6605516c846e04707e
gtapiero <giovanny.tapiero@pi.edu.co>
2026-02-26 16:38:13 -0500

    commit - GameDAO / .

--- src/db/GameDAO.java
+++ src/db/GameDAO.java
@@ -1,6 +1,27 @@
 package db;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 public class GameDAO {
-    public static void saveGame(String name, String name1, int score) {
+    public static void saveGame(
+            String player, String winner, int score) {
+
+        String sql =
+                "INSERT INTO DOMINO_GAME (PLAYER_NAME, WINNER, SCORE) VALUES (?, ?, ?)";
+
+        try (Connection con = OracleConnection.getConnection();
+             PreparedStatement ps = con.prepareStatement(sql)) {
+
+            ps.setString(1, player);
+            ps.setString(2, winner);
+            ps.setInt(3, score);
+            ps.executeUpdate();
+
+            System.out.println("📁 Partida registrada en Oracle");
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
