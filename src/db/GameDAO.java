b5e662de0aec749ebcaac23dca7e16a251bf6680
gtapiero <giovanny.tapiero@pi.edu.co>
2026-02-26 16:32:23 -0500

    commit - GameDAO .

--- src/db/GameDAO.java
+++ src/db/GameDAO.java
@@ -1,4 +1,6 @@
 package db;
 
 public class GameDAO {
+    public static void saveGame(String name, String name1, int score) {
+    }
 }
--- src/game/DominoGame.java
+++ src/game/DominoGame.java
@@ -16,6 +16,35 @@ public class DominoGame {
     }
 
     private void initialize() {
+        List<DominoTile> tiles = new ArrayList<>();
+        for (int i = 0; i <= 6; i++) {
+            for ( int j = i; j <= 6; j++) {
+                tiles.add(new DominoTile(i, j));
+            }
+        }
+        Collections.shuffle(tiles);
+        human.hand.addAll(tiles.subList(0, 7));
+        cpu.hand.addAll(tiles.subList(7, 14));
+        pile.add(tiles.get(14));
     }
 
+    public void play() {
+        System.out.println("Iniciando domino");
+        System.out.println("ficha inicial: " + pile.get(0));
+
+        Random r = new Random();
+        while (!human.hand.isEmpty() && !cpu.hand.isEmpty()) {
+            if (!human.hand.isEmpty()) human.hand.remove(0);
+            if (!cpu.hand.isEmpty()) cpu.hand.remove(0);
+        }
+
+        Player winner = human.hand.isEmpty() ? human : cpu;
+        System.out.println("🏆 Ganador: " + winner.name);
+
+        GameDAO.saveGame(
+                human.name,
+                winner.name,
+                winner.getScore()
+        );
+    }
 }
